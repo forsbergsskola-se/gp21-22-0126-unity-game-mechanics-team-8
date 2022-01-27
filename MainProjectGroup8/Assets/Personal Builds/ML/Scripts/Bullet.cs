cc4f125b428b6b5e09b69e7d11922bdcc1956bd5
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-01-27 15:42:47 +0100

    Moved files to new project folder

---
+++ MainProjectGroup8/Assets/Personal Builds/ML/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeSpan = 5.0f;
+    public Vector3 travelVector;
+    public float moveSpeed = 10.0f;
+
+    void Start()
+    {
+        StartCoroutine(DelayDestroy());
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            //Do thing Here
+        }
+        Destroy(gameObject);
+    }
+    
+
+    private IEnumerator DelayDestroy()
+    {
+        yield return new WaitForSeconds(lifeSpan);
+        Destroy(gameObject);
+    }
+    
+    
+    void Update()
+    {
+        transform.position += travelVector * moveSpeed * Time.deltaTime;
+    }
+}
