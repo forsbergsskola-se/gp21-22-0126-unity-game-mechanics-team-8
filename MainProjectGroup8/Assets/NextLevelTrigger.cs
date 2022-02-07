68bbb679e96275455496d9ca3cc94101c2214d1a
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-02-07 16:38:54 +0100

    Added next level trigger. Implemented in level 1 and 2

---
+++ MainProjectGroup8/Assets/NextLevelTrigger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class NextLevelTrigger : MonoBehaviour
+{
+	[SerializeField]
+	private SceneLoader sceneLoader;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			sceneLoader.LoadScene();
+		}
+	}
+}
